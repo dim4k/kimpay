9bf36bfa679050531e97459640e7442a3a6686fe
Antoine Kim <kima0012@gmail.com>
2025-12-07 20:56:17 +0100

    fix: fix pocketbase url

--- app/src/lib/pocketbase.ts
+++ app/src/lib/pocketbase.ts
@@ -4,9 +4,9 @@ import { browser } from '$app/environment';
 
 // If in browser, use localhost (or window.location if deployed). 
 // If on server (docker), use internal hostname.
-const url = browser 
+const url = env.PUBLIC_POCKETBASE_URL || (browser 
     ? (window.location.origin.includes('localhost') ? "http://localhost:8090" : "/") 
-    : (env.PUBLIC_POCKETBASE_URL || "http://pocketbase:8090");
+    : "http://pocketbase:8090");
 
 export const pb = new PocketBase(url);
 
